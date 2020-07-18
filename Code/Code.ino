9717c2403f05560cebab96d9cece61c7ae63aa2c
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-07-18 03:04:27 +0300

    Finished writing Code for the Project
    Everything Works like a charm

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,9 +1,83 @@
-void setup() {
+#include<Servo.h>
 
+
+const byte servoPin = 9;
+const byte trigPin = 2;
+const byte echoPin = 3;
+
+Servo myServo;
+long duration;
+int distance;
+byte triggerDistance = 15;
+bool triggered = 0;
+
+
+bool distance_triggered(){
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+
+  if (distance < triggerDistance){
+    return 1;  
+  }else{
+    return 0;  
+  }
+    
+}
+
+void open_box(){
+  
+  // Servo Position 10 :> Open
+  // Servo Position 130:> Close
+  Serial.println("Opening Box");
+  for (byte pos=130; pos>10; pos--){
+    myServo.write(pos);
+    delay(10);
+  }
+  delay(2000);
+  close_box();
 
 }
 
-void loop() {
+void close_box(){
+  
+  // Servo Position 10 :> Open
+  // Servo Position 130:> Close
+  Serial.println("Closing Box");
+  for (byte pos=10; pos<130; pos++){
+    myServo.write(pos);
+    delay(25);
+  }
 
+}
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+  myServo.attach(servoPin);
+  myServo.write(130);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
+}
+
+void loop() {
+  
+  triggered = distance_triggered();
+
+  if (triggered == 1){
+    open_box();
+  }
+
+  delay(100);
 
 }
