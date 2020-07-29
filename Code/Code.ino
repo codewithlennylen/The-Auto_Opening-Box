843cba8f0a92e595b5c852c4340d875032693789
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-07-29 19:38:14 +0300

    Update Code.ino
    Added a few Comments

--- Code/Code.ino
+++ Code/Code.ino
@@ -7,15 +7,16 @@ const byte trigPin = 2;
 const byte echoPin = 3;
 
 // Define Variables that will be used throughout the program
-Servo myServo;
-int openDelay = 2000; // time in milliseconds that the box will remain open
+Servo myServo; // Create an instance of the Servo
+int openDelay = 2000; // time in milliseconds that the box will remain open (2 Seconds)
 long duration;
 int distance;
 byte triggerDistance = 15; // If an obstacle's distance to the sensor is less than this value, Box opens
 bool triggered = 0; // Track the state of the Sensor; triggered or not?
 
 
-bool distance_triggered(){
+bool distance_triggered()
+{
   /*
    * This Function simply checks the distance. i.e the distance of an obstacle from the ultrasonic sensor
    * If the distance is less than the triggerDistance, the triggered boolean value is set to 1
@@ -24,10 +25,12 @@ bool distance_triggered(){
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
+
   // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
+
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance - This code returns distance in centimeters
@@ -35,7 +38,9 @@ bool distance_triggered(){
 
   if (distance < triggerDistance){
     return 1;  
-  }else{
+  }
+  else
+  {
     return 0;  
   }
     
@@ -50,13 +55,14 @@ void open_box(){
   // Servo Position 10 :> Open
   // Servo Position 130:> Close
   Serial.println("Opening Box");
-  for (byte pos=130; pos>10; pos--){
+  for (byte pos=130; pos>10; pos--)
+  {
     myServo.write(pos);
     delay(10);
   }
+
   delay(openDelay);
   close_box(); // Close the box after the Delay (openDelay)
-
 }
 
 void close_box(){
@@ -68,11 +74,11 @@ void close_box(){
   // Servo Position 10 :> Open
   // Servo Position 130:> Close
   Serial.println("Closing Box");
-  for (byte pos=10; pos<130; pos++){
+  for (byte pos=10; pos<130; pos++)
+  {
     myServo.write(pos);
     delay(25);
   }
-
 }
 
 void setup() {
@@ -91,10 +97,11 @@ void loop() {
   
   triggered = distance_triggered();
 
-  if (triggered == 1){
+  // Check if there's an object close to the ultrasonic sensor then open the Box
+  if (triggered == 1)
+  {
     open_box();
   }
 
   delay(100);
-
 }
